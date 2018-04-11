cea2c52b4b1514056a62c6bd004968a0f47640e4
Unknown <adriankamulegeya@gmail.com>
2018-04-11 16:19:03 +0100

    Added pick up mechanic and looking at witch mechanic

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public List<GameObject> inventory;
+	private Rigidbody rb;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		inventory = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+}
---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+
+	private Player player;
+	private bool canShowHoverText = false;
+	private bool lookingAtWitch = false;
+	public float maxPickUpDistance = 10.0f;
+	public float maxWitchDistance = 50.0f;
+	public float timeLookingAtWitch = 0.0f;
+	public float maxTimeToLook = 4f;
+
+
+	void Awake()
+	{
+		player = GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		
+		//checks if we see an object
+		if (Physics.Raycast(transform.position, forward, out hit))
+		{
+
+			if (hit.distance <= maxWitchDistance && hit.collider.gameObject.CompareTag("Witch"))
+			{
+				lookingAtWitch = true;
+			}
+			else
+			{
+				lookingAtWitch = false;
+			}
+			
+			//checks if we're close enough to pick it up
+			if (hit.distance <= maxPickUpDistance && hit.collider.gameObject.CompareTag("Pick Up"))
+			{
+
+				canShowHoverText = true;
+				
+				if (Input.GetMouseButtonDown(0))
+				{
+					player.inventory.Add(hit.collider.gameObject);	//adds item to inventory
+					hit.collider.gameObject.SetActive(false);		// can probably do other stuff later
+					Debug.Log("Picked Up");
+				}
+			}
+			else
+			{
+				canShowHoverText = false;
+			}
+		}
+
+		//adds time to how long you can look at the witch for
+		//resets if you turn away
+		if (lookingAtWitch)
+		{
+			timeLookingAtWitch += Time.deltaTime;
+			if (timeLookingAtWitch > maxTimeToLook)		//look too long it hurts you (In text how frightening)
+			{
+				Debug.Log("Ouch");
+				timeLookingAtWitch = 0.0f;
+			}
+		}
+		else
+		{
+			timeLookingAtWitch = 0.0f;
+		}
+	}
+	
+	private void OnGUI()
+	{
+		if (canShowHoverText)
+		{
+			GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 150, 20), "Click to pick up");
+		}
+	}
+	
+}
